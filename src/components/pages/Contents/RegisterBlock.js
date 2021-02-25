ef7e81b16f3dbbc10ff60d45b5b390e8a0d93b83
Aunggool <58565065+Aunggool@users.noreply.github.com>
2021-02-25 19:38:43 +0700

    Update RegisterBlock.js

--- src/components/pages/Contents/RegisterBlock.js
+++ src/components/pages/Contents/RegisterBlock.js
@@ -12,9 +12,11 @@ function FourthContent() {
               อีเมลล์
             </label>
             <input className="home__hero-register-email" type="text" />
-            <button className="home__hero-register-button">
-              สมัครใช้งาน
+            <a href='https://app.seanheng.com/web.merchantmgmt.biz/register?q=wzF+X4jNJESoNmPzahaeWRaCAXeNqlDC9waf11Yf3x405p7UaFmWPXEec4msKeja6yknnZw8M/Xf30LnAlC3JY645KX09ZhBaQANDju26PE=' className="nav-links" >
+              <button className="home__hero-register-button" >
+                สมัครใช้งาน
             </button>
+            </a>
           </div>
         </div>
       </div>
