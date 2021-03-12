40f4f107ea95cbb16feac1238d85a0e39a6d5ecb
toeyaunggool <aunggool.srisang@gmail.com>
2021-03-12 20:33:44 +0700

    add id footer

--- src/components/pages/Footer/Footer.js
+++ src/components/pages/Footer/Footer.js
@@ -9,34 +9,34 @@ function Footer() {
     <div className="footer-container">
       <div className="footer__main-wrapper">
         <div className="footer__text-wapper facebook">
-          <a className="social-logo" href="https://www.facebook.com/seanhengchatbot">
+          <a className="social-logo" href="https://www.facebook.com/seanhengchatbot" id='fb_footer'>
             <FaFacebook />
           </a>
-          <a className="footer__text" href="https://www.facebook.com/seanhengchatbot">
+          <a className="footer__text" href="https://www.facebook.com/seanhengchatbot" id='fb_footer'>
             เซียนเฮงแชทบอท
           </a>
         </div>
         <div className="footer__text-wapper line">
-          <a className="social-logo" href="https://line.me/R/ti/p/@SeanHeng">
+          <a className="social-logo" href="https://line.me/R/ti/p/@SeanHeng" id='line_footer'>
             <FaLine />
           </a>
-          <a className="footer__text" href="https://line.me/R/ti/p/@SeanHeng">
+          <a className="footer__text" href="https://line.me/R/ti/p/@SeanHeng" id='line_footer'>
             @SeanHeng
           </a>
         </div>
         <div className="footer__text-wapper phone">
-          <aside className="social-logo" href="tel:0931646553">
+          <aside className="social-logo" href="tel:0931646553" id='mobile_footer'>
             <FaPhoneAlt />
           </aside>
-          <a className="footer__text" href="tel:0931646553">
+          <a className="footer__text" href="tel:0931646553" id='mobile_footer'>
             093-164-6553
           </a>
         </div>
         <div className="footer__text-wapper email">
-          <a className="social-logo" href="mailto:support.admin@seanheng.com">
+          <a className="social-logo" href="mailto:support.admin@seanheng.com" id='email_footer'>
             <MdEmail />
           </a>
-          <a className="footer__text" href="mailto:support.admin@seanheng.com">
+          <a className="footer__text" href="mailto:support.admin@seanheng.com" id='email_footer'>
             support.admin@seanheng.com
           </a>
         </div>
